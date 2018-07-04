cd3295c816afa1320e32a8e46315527525efbb57
wangyun <w295534955>
2018-07-04 11:21:09 +0800

    通用模块已经更新

---
+++ modulecommon/src/main/java/com/ys/modulecommon/utils/UpdateUtil.java
@@ -0,0 +1,11 @@
+package com.ys.modulecommon.utils;
+
+/**
+ * Created by Administrator on 2018/7/4.
+ */
+public class UpdateUtil {
+    public static void update()
+    {
+       LogUtil.e("TAG","通用模块已更新");
+    }
+}
